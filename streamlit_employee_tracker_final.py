d44dcf9c4fbf61f76086b60974f9ed2d5f39fa05
Devesh9213 <deveshtiwari@pixsedit.com>
2025-03-29 11:40:55 +0100

    Update streamlit_employee_tracker_final.py

--- streamlit_employee_tracker_final.py
+++ streamlit_employee_tracker_final.py
@@ -11,8 +11,8 @@ from pathlib import Path
 import pandas as pd
 import plotly.express as px
 import time
-from streamlit.components.v1 import html
-
+from streamlit.components.v1 import HTML
+from streamlit_autorefresh import st_autorefresh 
 
 # ====================
 # CONFIGURATION
